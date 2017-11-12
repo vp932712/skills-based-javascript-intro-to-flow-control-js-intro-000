a46904c09c477db70bd30c515f56ae2c905a2daf
Vaibhav Patel <vp932712@gmail.com>
2017-11-12 22:12:07 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -6,8 +6,10 @@ return ("You are a teenager!")
 }
 
 function teenager(age) {
-if (age>=13 && age <==19) return ("You are a teenager!")
-return ("You are not a teenager!")
+if (age>=13 && age <==19) {
+  return ("You are a teenager!")
+} else {
+  return ("You are not a teenager!")
 }
 
 // function ageChecker(age) {
